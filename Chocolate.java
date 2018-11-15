e3ca5bc984290c8feafd61bea1731dbd5e926db3
Phakjira <phakjra2000@gmail.com>
2018-11-15 14:21:04 +0700

    made changes

---
+++ Chocolate.java
@@ -0,0 +1,5 @@
+public class Chocolate{
+	public static void main(String[] args){
+		System.out.println("Choco late Choco Moca Late");
+	}
+}
--- HiJess.java
+++ HiJess.java
@@ -1,5 +1,6 @@
 public class HiJess{
 	public static void main(String[] args){
 		System.out.println("Hi Jess");
+		System.out.println("Welcome to Jessie's World");
 	}
 }
